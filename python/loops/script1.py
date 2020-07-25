fd494d9e29cd631d6ffd208a4ef9992a9b4a22a5
Sahan Dissanayaka <t.sahan998@gmail.com>
2020-07-25 10:14:25 +0530

    update script

--- python/loops/script1.py
+++ python/loops/script1.py
@@ -6,7 +6,7 @@ for index, a in enumerate(areas) :
     print("room "+str(index)+": "+str(a))
     
     
-# non programming floks
+# non programming folks
 # areas list
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 

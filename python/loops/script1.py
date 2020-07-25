a28259abafc1ae2f6e4a8af1d7111f9fca14d048
Sahan Dissanayaka <t.sahan998@gmail.com>
2020-07-25 10:13:51 +0530

    change script

--- python/loops/script1.py
+++ python/loops/script1.py
@@ -4,3 +4,12 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 # Change for loop to use enumerate() and update print()
 for index, a in enumerate(areas) :
     print("room "+str(index)+": "+str(a))
+    
+    
+# non programming floks
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for index, area in enumerate(areas) :
+    print("room " + str(index+1) + ": " + str(area))

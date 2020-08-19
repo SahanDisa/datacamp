61e655268c34ff40449af1ff9d1a17c153ede30e
Sahan Dissanayaka <t.sahan998@gmail.com>
2020-08-19 23:27:39 +0530

    Update script1.py

--- python/list_comprehension/script1.py
+++ python/list_comprehension/script1.py
@@ -16,3 +16,13 @@ new_fellowship = [member for member in fellowship if len(member)>=7]
 
 # Print the new list
 print(new_fellowship)
+
+#same code with different condition
+# Create a list of strings: fellowship
+fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
+
+# Create list comprehension: new_fellowship
+new_fellowship = [member if len(member)>= 7 else "" for member in fellowship]
+
+# Print the new list
+print(new_fellowship)
